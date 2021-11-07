7fbecf23339114cd35fc6098459d4724d6b3c79f
Lan Jiang <jianglan1350@gmail.com>
2021-11-07 13:46:36 +0100

    submit first version

--- scripts/bash/run-aggrecol.sh
+++ scripts/bash/run-aggrecol.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh ./run-aggrecol-luigi-task.sh -d ../../data/dataset.jl.gz -r ../../results/ -o All -c 0.7 -t 900 -s 3
+sh ./run-aggrecol-luigi-task.sh -d ../../data/dataset.jl.gz -r ../../results/ -o All -c 0.7 -t 300 -s 3
